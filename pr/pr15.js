40db6a4fdfb09dc215c92079ab2b1981c864824d
WARDKEY <tjrmsduq@naver.com>
2025-05-10 16:57:12 +0900

    Javascript 자료형 실습

---
+++ pr/pr15.js
@@ -0,0 +1,13 @@
+let obj = {
+  name: "홍길동",
+  age: 13,
+  like: "아버지",
+};
+
+console.log(obj);
+
+let mathScore = "77";
+let engScore = "88";
+
+let avgScore = (Number(mathScore) + Number(engScore))/2;
+console.log(avgScore);
