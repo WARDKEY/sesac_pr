ef080043b006e960468ee70d0241575073707448
WARDKEY <tjrmsduq@naver.com>
2025-06-09 18:40:52 +0900

    fix: gitignore 수정

--- node/app.js
+++ node/app.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/user", userRouter);
+app.use("/post", postRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, `포트로 서버가 열림`);
