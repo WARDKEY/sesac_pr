86de285d839e069f893d6a46313eaf4ea50c3b59
WARDKEY <tjrmsduq@naver.com>
2025-06-09 18:37:25 +0900

    feat: 프로젝트 설정 및 user 라우터 생성

---
+++ node/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userRouter from "./routes/user.router.js";
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/user", userRouter);
+
+app.listen(PORT, () => {
+  console.log(PORT, `포트로 서버가 열림`);
+});
